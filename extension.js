3f1537c12b7db05e8019734f5cfe5c1bb6abe4a4
Alen Šiljak <code@alensiljak.eu.org>
2021-02-22 11:48:39 +0100

    adding a web view provider

--- extension.js
+++ extension.js
@@ -1,12 +1,12 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
-let abcjs = require('abcjs');
+const vscode = require("vscode");
+let abcjs = require("abcjs");
 //import abcjs from 'abcjs'
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
-let panel = null
+let panel = null;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -15,48 +15,73 @@ let panel = null
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "abcjs-editor" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "abcjs-editor" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "abcjs-editor.helloWorld",
+    function () {
+      // The code you place here will be executed every time your command is executed
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('abcjs-editor.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+      // Display a message box to the user
+      vscode.window.showInformationMessage("Hello World from abcjs editor!");
+    }
+  );
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from abcjs editor!');
-	});
+  context.subscriptions.push(disposable);
 
-	context.subscriptions.push(disposable);
+  // abcjs preview
 
-	const outputChannel = vscode.window.createOutputChannel('abcjs-editor errors');
-	
-	// Show the viewer
-	let viewer = vscode.commands.registerCommand('abcjs-editor.showPreview', 
-		async () => await showPreview(context, outputChannel));
-	context.subscriptions.push(viewer);
+  //const outputChannel = vscode.window.createOutputChannel('abcjs-vscode errors');
+  // Show the viewer
+  // let viewer = vscode.commands.registerCommand('abcjs-vscode.showPreview',
+  // 	async () => await showPreview(context, outputChannel));
+  // context.subscriptions.push(viewer);
+
+  // Web View preview
+  // https://github.com/microsoft/vscode-extension-samples/blob/master/webview-view-sample/src/extension.ts
+  const provider = new AbcjsPreviewProvider(context.extensionUri);
+
+  context.subscriptions.push(
+    vscode.window.registerWebviewViewProvider(
+      AbcjsPreviewProvider.viewType,
+      provider
+    )
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "abcjs-vscode.showPreview",
+      provider.renderAbc
+    )
+  );
 }
 
 /**
  * open the preview window to the side.
  */
 async function showPreview(context, outputChannel) {
-	//vscode.window.showInformationMessage('Opening the preview in abcjs editor!');
-	panel = vscode.window.createWebviewPanel('musicSheet', 'Music Sheet', vscode.ViewColumn.Beside, {
-		enableScripts: true
-	});
+  //vscode.window.showInformationMessage('Opening the preview in abcjs editor!');
+  panel = vscode.window.createWebviewPanel(
+    "musicSheet",
+    "Music Sheet",
+    vscode.ViewColumn.Beside,
+    {
+      enableScripts: true,
+    }
+  );
 
-	// panel.webview.html = getWebviewContent(
-	// 	getNormalizedEditorContent(vscode.window.activeTextEditor), context.extensionPath);
-	const dom = new JSDOM(`<!DOCTYPE html><html><body>
+  // panel.webview.html = getWebviewContent(
+  // 	getNormalizedEditorContent(vscode.window.activeTextEditor), context.extensionPath);
+  const dom = new JSDOM(`<!DOCTYPE html><html><body>
 		<div id="paper"></div>
 	</body></html>`);
-	console.log(dom.window.document.querySelector("p").textContent); // "Hello world"
+  console.log(dom.window.document.querySelector("p").textContent); // "Hello world"
 
-	var abc = `X: 1
+  var abc = `X: 1
 	T: Cooley's
 	M: 4/4
 	L: 1/8
@@ -64,27 +89,27 @@ async function showPreview(context, outputChannel) {
 	|:D2|EB{c}BA B2 EB|~B2 AB dBAG|FDAD BDAD|FDAD dAFD|
 	EBBA B2 EB|B2 AB defg|afe^c dBAF|DEFD E2:|
 	|:gf|eB B2 efge|eB B2 gedB|A2 FA DAFA|A2 FA defg|
-	eB B2 eBgB|eB B2 defg|afe^c dBAF|DEFD E2:|`
-	try {
-	const abcObject = await abcjs.renderAbc("paper", abc)
-	console.log(abcObject)
-	} catch(error) {
-		console.error(error)
-	}
+	eB B2 eBgB|eB B2 defg|afe^c dBAF|DEFD E2:|`;
+  try {
+    const abcObject = await abcjs.renderAbc("paper", abc);
+    console.log(abcObject);
+  } catch (error) {
+    console.error(error);
+  }
 
-	panel.webview.html = ""
+  panel.webview.html = "";
 
-	// handle messages from the webview
-	// panel.webview.onDidReceiveMessage(message => {
-	// 	switch (message.command) {
-	// 		case 'selection':
-	// 			jumpToPosition(message.start, message.stop);
-	// 			return;
-	// 	}
-	// }, undefined, context.subscriptions);	
-	panel.webview.onDidReceiveMessage(message => {
-		console.log(message)
-	})
+  // handle messages from the webview
+  // panel.webview.onDidReceiveMessage(message => {
+  // 	switch (message.command) {
+  // 		case 'selection':
+  // 			jumpToPosition(message.start, message.stop);
+  // 			return;
+  // 	}
+  // }, undefined, context.subscriptions);
+  panel.webview.onDidReceiveMessage((message) => {
+    console.log(message);
+  });
 }
 
 // function getNormalizedEditorContent(editor) {
@@ -104,7 +129,92 @@ async function showPreview(context, outputChannel) {
 // this method is called when your extension is deactivated
 function deactivate() {}
 
-module.exports = {
-	activate,
-	deactivate
+class AbcjsPreviewProvider {
+  constructor(extensionUri) {
+    this._view = null;
+    this._extensionUri = extensionUri;
+
+    console.log("extension uri:", extensionUri);
+  }
+
+  static viewType = "abcjs-vscode.abcView";
+
+  resolveWebviewView(webviewView, context, _token) {
+    this._view = webviewView;
+
+    webviewView.webview.options = {
+      // Allow scripts in the webview
+      enableScripts: true,
+
+      localResourceRoots: [this._extensionUri],
+    };
+
+    webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+
+    webviewView.webview.onDidReceiveMessage((data) => {
+      switch (data.type) {
+        case "colorSelected": {
+          vscode.window.activeTextEditor?.insertSnippet(
+            new vscode.SnippetString(`#${data.value}`)
+          );
+          break;
+        }
+      }
+    });
+  }
+
+  renderAbc() {
+    console.log("rendering output");
+  }
+
+  _getHtmlForWebview(webview) {
+    // Get the local path to main script run in the webview, then convert it to a uri we can use in the webview.
+    const scriptUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "main.js")
+    );
+
+    // Do the same for the stylesheet.
+    const styleResetUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "reset.css")
+    );
+    const styleVSCodeUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "vscode.css")
+    );
+    const styleMainUri = webview.asWebviewUri(
+      vscode.Uri.joinPath(this._extensionUri, "media", "main.css")
+    );
+
+    // Use a nonce to only allow a specific script to be run.
+    //const nonce = getNonce();
+    const nonce = "once?";
+
+    return `<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<!--
+					Use a content security policy to only allow loading images from https or from our extension directory,
+					and only allow scripts that have a specific nonce.
+				-->
+				<meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<link href="${styleResetUri}" rel="stylesheet">
+				<link href="${styleVSCodeUri}" rel="stylesheet">
+				<link href="${styleMainUri}" rel="stylesheet">
+				
+				<title>Cat Colors</title>
+			</head>
+			<body>
+				<ul class="color-list">
+				</ul>
+				<button class="add-color-button">Add Color</button>
+				<script nonce="${nonce}" src="${scriptUri}"></script>
+			</body>
+			</html>`;
+  }
 }
+
+module.exports = {
+  activate,
+  deactivate,
+};
